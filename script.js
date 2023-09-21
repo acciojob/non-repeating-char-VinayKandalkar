0bdc9606ffabbecfd4218625d7e6c907c61bf667
VinayKandalkar <142089926+VinayKandalkar@users.noreply.github.com>
2023-09-21 23:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function firstNonRepeatedChar(str) {
- let const = {}
+ let obj = {}
  for (let i = 0; i < str.length; i++) {
-	 if (const[str[i]] !== undefined) {
+	 if (obj[str[i]] !== undefined) {
 		 // obj["a"]
-		obj [str[i]] += 1;
+		obj[str[i]] += 1;
 	 } else {
-		 const[str[i]] = 1;
+		 obj[str[i]] = 1;
 	   } 
 	}
 	for (let i = 0; i < str.length; i++) {
-		if (const[str[i]] == 1) {
+		if (obj[str[i]] == 1) {
 			return str[i];
 		}
 	}
